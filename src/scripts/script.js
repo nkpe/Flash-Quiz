db7476d403d3394e116a6e67f71ff2cc396a3dfe
Naomi <63055040+nkpe@users.noreply.github.com>
2022-07-21 14:30:45 +0100

    if else js statments

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,28 +3,49 @@ const allAnswerOptions = document.getElementsByClassName("qu-option");
 const submitBtn = document.getElementById("quiz-submit");
 const quizResult = document.getElementById("quiz-result");
 
-
 let userCorrectAnswers = [];
 
 quizResult.style.display = "none";
 
 let clickCheck = (e) => {
-    console.log(e);
+    console.log("e.target = " , e.target);
+    let clickedElement = e.target;
+    let clickedName = e.target.name;
+
+    if (userCorrectAnswers.length === 0){
+        console.log("first if hit");
+        userCorrectAnswers.push(e.target);
+    } else {
+        for (i=0; i<userCorrectAnswers.length; i++){
+            if (userCorrectAnswers[i].name === clickedName){
+                console.log("reassignment");
+                userCorrectAnswers[i] = clickedElement;
+            } else {
+                console.log("push to array");
+                userCorrectAnswers.push(e.target);
+            };
+        };
+    };
+    
+    console.log(userCorrectAnswers);
+
 };
 
-let correctAnswers = (e) =>{
-    console.log(e);
-    let clickedClasses = e.target.classList;
-    if (clickedClasses.contains("qu-correct")){
-        console.log("This is Correct");
-        userCorrectAnswers.push(e.target.id);
-    } else {
-        console.log("Incorrect")
-    };
-};
+// let correctAnswers = (e) =>{
+//     clickCheck(e);
+//     console.log(e);
+//     let clickedClasses = e.target.classList;
+//     if (clickedClasses.contains("qu-correct")){
+//         console.log("This is Correct");
+//         userCorrectAnswers.push(e.target);
+//     } else {
+//         console.log("Incorrect")
+//     };
+//     console.log(userCorrectAnswers);
+// };
 
 for (option of allAnswerOptions){
-    option.addEventListener('click' , correctAnswers);
+    option.addEventListener('click' , clickCheck);
 };
 
 let validateAnswers = () => {
