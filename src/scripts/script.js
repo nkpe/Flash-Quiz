4d3a621e2f1220de419bf71dc0f2326efbc8865d
Naomi <63055040+nkpe@users.noreply.github.com>
2022-07-21 15:19:06 +0100

    Quiz validation using Maps

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,64 +3,50 @@ const allAnswerOptions = document.getElementsByClassName("qu-option");
 const submitBtn = document.getElementById("quiz-submit");
 const quizResult = document.getElementById("quiz-result");
 
-let userCorrectAnswers = [];
+// let userCorrectAnswers = [];
 
 quizResult.style.display = "none";
+const clickMap = new Map();
 
-let clickCheck = (e) => {
-    console.log("e.target = " , e.target);
-    let clickedElement = e.target;
-    let clickedName = e.target.name;
-
-    if (userCorrectAnswers.length === 0){
-        console.log("first if hit");
-        userCorrectAnswers.push(e.target);
-    } else {
-        for (i=0; i<userCorrectAnswers.length; i++){
-            if (userCorrectAnswers[i].name === clickedName){
-                console.log("reassignment");
-                userCorrectAnswers[i] = clickedElement;
-            } else {
-                console.log("push to array");
-                userCorrectAnswers.push(e.target);
-            };
-        };
-    };
-    
-    console.log(userCorrectAnswers);
-
+let clickMapSet = (e) => {
+    clickMap.set(e.target.name , e.target.classList);
 };
-
-// let correctAnswers = (e) =>{
-//     clickCheck(e);
-//     console.log(e);
-//     let clickedClasses = e.target.classList;
-//     if (clickedClasses.contains("qu-correct")){
-//         console.log("This is Correct");
-//         userCorrectAnswers.push(e.target);
-//     } else {
-//         console.log("Incorrect")
-//     };
-//     console.log(userCorrectAnswers);
-// };
 
 for (option of allAnswerOptions){
-    option.addEventListener('click' , clickCheck);
+    option.addEventListener('click' , clickMapSet);
 };
 
-let validateAnswers = () => {
-    let percentCorrect = (userCorrectAnswers.length / allQuestions.length) * 100;
+let correctAnswers = (e) => {
+    let mapValues = clickMap.values();
+    let resultsArray = [];
+    for (let value of mapValues){
+        let results = value.contains("qu-correct");
+        resultsArray.push(results);
+    };
 
-    console.log(percentCorrect);
+    let counter = 0;
+
+    for (let result of resultsArray){
+        if (result === true){
+            counter++;
+        }
+    };
+    validateAnswers(counter);
+};
+
+let validateAnswers = (counter) => {
+    let percentCorrect = (counter / allQuestions.length) * 100;
     quizResult.style.display = "block";
     if (percentCorrect === 100){
         quizResult.innerHTML = `<p>You got ${percentCorrect}%. Well Done!</p>`;
     } else {
-        quizResult.innerHTML = `<p>You got ${percentCorrect}%. Would you like to try again?</p>`;
+        quizResult.innerHTML = `<p>You got ${Math.round(percentCorrect)}%. Would you like to try again?</p>`;
     };
 };
 
-submitBtn.addEventListener('click' , validateAnswers);
+submitBtn.addEventListener('click' , correctAnswers);
+
+
 
 
 
